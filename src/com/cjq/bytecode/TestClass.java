54c35df439c440a146620139892d70b051489629
chenjunqi <chenjunqi@thunisoft.com>
2022-06-21 13:53:27 +0800

    add test bytecode

---
+++ src/com/cjq/bytecode/TestClass.java
@@ -0,0 +1,23 @@
+/*
+ * @projectName CjqOutOfMemoryForJ6
+ * @package com.cjq.bytecode
+ * @className com.cjq.bytecode.TestClass
+ * @copyright Copyright 2022 Thuisoft, Inc. All rights reserved.
+ */
+package com.cjq.bytecode;
+
+/**
+ * TestClass
+ * @description 测试编译的字节码
+ * @author chenjunqi
+ * @date 2022年06月21日 13:35
+ * @version 1.2.0
+ */
+public class TestClass {
+
+    private int m;
+
+    public int inc(){
+        return m + 1;
+    }
+}
